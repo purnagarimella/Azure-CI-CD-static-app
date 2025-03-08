e3c7c890dce23983b262a29b159e4aeff8b23c35
Purna Garimella <garimellapurna@gmail.com>
2025-03-08 11:02:23 -0600

    fixed AB#4

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
 
-    <h1>Gen Ai Every Where</h1>
+    <h1>Gen Ai Every Where :)</h1>
 	<h2>Upload your data to get Gen Ai predictions</h2>
     <img
       alt="Vue logo"
